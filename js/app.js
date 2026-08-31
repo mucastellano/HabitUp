fc5df3444a697f5c326598feb2054661bdc42ab0
mucastellano <murilocastellano10@gmail.com>
2026-08-31 13:44:25 -0300

    feat: "Adiciona sistema de niveis e barras de XP"

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
 let xp = 0;
 let sequencia = 0;
- 
+let nivel = 1;
+
 const xpTexto = document.getElementById("xp");
 const sequenciaTexto = document.getElementById("sequencia");
 const botaoConcluir = document.getElementById("botao-concluir");
+const nivelTexto = document.getElementById("nivel");
+const progressoXp = document.getElementById("progresso-xp");
 
 botaoConcluir.onclick = function () {
     xp = xp + 20;
     sequencia = sequencia + 1;
+    if (xp >= 100) {
+        nivel = nivel + 1;
+        xp = 0;
+        nivelTexto.textContent = "Nível: " + nivel;
+        progressoXp.style.width = "%";
+    }
     xpTexto.textContent = `XP: ${xp} / 100`;
     sequenciaTexto.textContent = `🔥 Sequência: ${sequencia} dias`;
 };
